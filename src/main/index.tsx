23229f997b55dfb1d0c732271efa4eeb888c1d8a
Kussagra Pathak <67318693+kush11251@users.noreply.github.com>
2026-03-19 05:10:49 +0530

    feat: integrate src/main/index.tsx

---
+++ src/main/index.tsx
@@ -0,0 +1,12 @@
+// @ts-check
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
