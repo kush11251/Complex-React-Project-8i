4ebbe3f1d4ce325e12e079de7083ca42c451971b
Kussagra Pathak <67318693+kush11251@users.noreply.github.com>
2026-03-19 05:10:52 +0530

    feat: integrate src/components/Home.tsx

---
+++ src/components/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from './ui/buttons/Button';
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to the Home page</h1>
+      <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default Home;
